b754e7baa64ce4dfcc3e816285ab9f1f3fe0b4da
Asia <ziola.jm@gmail.com>
2025-09-30 08:56:33 +0200

    lesson10

---
+++ lesson10-prime-numbers/countFactors.ts
@@ -0,0 +1,17 @@
+function countFactorsSolution(N: number): number {
+  let count = 0;
+  let i = 1;
+
+  while (i * i < N) {
+    if (N % i === 0) {
+      count += 2;
+    }
+    i++;
+  }
+
+  if (i * i === N) {
+    count += 1;
+  }
+
+  return count;
+}
